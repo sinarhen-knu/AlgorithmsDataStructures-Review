cd21b8e985519a5134ce4cd4a0cc4ec21f1023d3
sinarhen-knu <105736826+sinarhen@users.noreply.github.com>
2024-05-07 23:42:32 +0300

    Config upd

--- site/.vitepress/config.js
+++ site/.vitepress/config.js
@@ -9,16 +9,24 @@ export default defineConfig({
   base: "/AlgorithmsDataStructures-Review/",
 
   themeConfig: {
-    nav: [{ text: "Лабораторні", link: "/labs/1" }],
+    nav: [{ text: "Лабораторні", link: "/labs/1" }, { text: "Теорія", link: "/theory/1" }],
 
     sidebar: [
       {
         text: "Лабораторні роботи",
         items: [
-            { text: "Лабораторна робота №1", link: "/labs/1" },
+          { text: "Лабораторна робота №1", link: "/labs/1" },
           { text: "Лабораторна робота №2", link: "/labs/2" },
           { text: "Лабораторна робота №4", link: "/labs/4" },
         ],
+      },
+      {
+        text: "Теоретичні питання",
+        items: [
+          { text: "Теоретичне питання до ЛР №1", link: "/theory/1" },
+          { text: "Теоретичне питання до ЛР №2", link: "/theory/2" },
+          { text: "Теоретичне питання до ЛР №4", link: "/theory/4" }
+        ]
       }
     ],
 
